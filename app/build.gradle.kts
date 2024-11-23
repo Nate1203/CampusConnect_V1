ed398892b9c07c6015d85ccf0e4d9e44fb3768b6
Nathan <st10039749@vcconnect.edu.za>
2024-11-23 13:51:48 +0200

    Added new implication and import to gradel, for chat messaing function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.viewpager2)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.room.ktx)
 
     // Testing dependencies
     testImplementation(libs.junit)
